f943b5b9cee725d6192486e36034f5affe8dcb3b
yurytrifonov <jutrifonov@uchicago.edu>
2024-03-25 10:59:06 -0500

    upd includes the default behavior for Ng = NULL

--- example.R
+++ example.R
@@ -23,18 +23,19 @@ set.seed(123) # fix the random seed
 
 # Generate a pseudo-random sample with clusters and two treatments = c(0.2, 0.8)
 data <- sreg.rgen(
-  n = 10000, tau.vec = c(0.2, 0.8),
+  n = 100, tau.vec = c(0.2, 0.8),
   n.strata = 4, cluster = T, Nmax = 50
 )
 Y <- data$Y
 S <- data$S
 D <- data$D
-# X <- data.frame("x_1" = data$x_1, "x_2" = data$x_2)
-X <- data.frame("Ng" = data$Ng, "x_1" = data$x_1, "x_2" = data$x_2)
+X <- data.frame("x_1" = data$x_1, "x_2" = data$x_2)
+#X <- data.frame("Ng" = data$Ng, "x_1" = data$x_1, "x_2" = data$x_2)
 # X <- data.frame("Ng" = data$Ng)
 Ng <- data$Ng
+Ng[1:30] <- 40
 G.id <- data$G.id
 
 # Estimate the ATE, s.e., etc.
-test <- sreg(Y, S, D, G.id, Ng, X = X, HC1 = TRUE)
+test <- sreg(Y, S = S, D, G.id = G.id, Ng = NULL, X = X)
 test <- sreg(Y, S = NULL, D, G.id, Ng, X = NULL, HC1 = FALSE)
