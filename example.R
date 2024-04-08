a2bec8aa5788e9ddfcc99ee095198cc849d4e346
yurytrifonov <jutrifonov@uchicago.edu>
2024-04-08 18:14:05 -0500

    minor exmpl upd

--- example.R
+++ example.R
@@ -3,7 +3,7 @@ install_github("yurytrifonov/sreg") # install sreg
 library(devtools) # install devtools
 library(sreg) # install sreg
 packageDescription("sreg") # package description
-# ?sreg # R documentation for sreg()
+ ?sreg # R documentation for sreg()
 # ?sreg.rgen # R documentation for sreg.rgen()
 # %#%#%#%#%#%#%#%#%#%
 # %#%#%#%#%#%#%#%#%#%
@@ -22,7 +22,7 @@ test <- sreg(Y, S, D, G.id = NULL, Ng = NULL, X = X)
 set.seed(123) # fix the random seed
 # Generate a pseudo-random sample with clusters and two treatments = c(0.2, 0.8)
 data <- sreg.rgen(
-  n = 1000, tau.vec = c(0.2, 0.8),
+  n = 100, tau.vec = c(0.2, 0.8),
   n.strata = 4, cluster = T, Nmax = 50
 )
 Y <- data$Y
