be667188e7d25e7761b2afd4a378548c7ad61e50
yurytrifonov <jutrifonov@uchicago.edu>
2024-04-18 13:07:26 -0500

    debug the global var pi_hat solve 'no visible binding for global variable ‘pi_hat’'

--- R/creg.R
+++ R/creg.R
@@ -92,17 +92,17 @@ pi.hat.creg <- function(S, D, inverse = FALSE)
   n <- length(D)
   data <- data.frame(S, D)
   counts <- data %>%
-    group_by(S, D) %>%
+    group_by(.data$S, .data$D) %>%
     summarise(n = n())
   scount <- data %>%
-    group_by(S) %>%
+    group_by(.data$S) %>%
     summarise(ns = n())
 
   j <- left_join(counts, scount, by = join_by(S == S))
   j$pi_hat <- j$n / j$ns
   pi_hat_all <- j %>%
-    select(c(S, D, pi_hat)) %>%
-    spread(key = D, value = pi_hat)
+    select(c(.data$S, .data$D, .data$pi_hat)) %>%
+    spread(key = .data$D, value = .data$pi_hat)
   if (inverse) {
     n_repeat <- max(counts$D)
     ret_df <- matrix(replicate(n_repeat, pi_hat_all$"0"), nrow = nrow(pi_hat_all))
