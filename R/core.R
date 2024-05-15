a356de853066cd2d36f18b6b206927fd0a023e4b
yurytrifonov <jutrifonov@uchicago.edu>
2024-05-15 11:58:44 -0500

    upd description in the core file

--- R/core.R
+++ R/core.R
@@ -19,12 +19,12 @@
 #'
 #' @return An object of class \code{sreg} that is a list containing the following elements:
 #' \itemize{
-#' \item \code{tau.hat}: a \eqn{1 \times |\mathcal A|} vector of ATE estimates, where \eqn{|\mathcal A|} represents the number of treatments
-#' \item \code{se.rob}: a \eqn{1 \times |\mathcal A|} vector of standard errors estimates, where \eqn{|\mathcal A|} represents the number of treatments
-#' \item \code{t.stat}: a \eqn{1 \times |\mathcal A|} vector of \eqn{t}-statistics, where \eqn{|\mathcal A|} represents the number of treatments
-#' \item \code{p.value}: a \eqn{1 \times |\mathcal A|} vector of corresponding \eqn{p}-values, where \eqn{|\mathcal A|} represents the number of treatments
-#' \item \code{CI.left}: a \eqn{1 \times |\mathcal A|} vector of the left bounds of the 95\% as. confidence interval
-#' \item \code{CI.right}: a \eqn{1 \times |\mathcal A|} vector of the right bounds of the 95\% as. confidence interval
+#' \item \code{tau.hat}: a \eqn{1 \times |\mathcal A|} \code{vector} of ATE estimates, where \eqn{|\mathcal A|} represents the number of treatments
+#' \item \code{se.rob}: a \eqn{1 \times |\mathcal A|} \code{vector} of standard errors estimates, where \eqn{|\mathcal A|} represents the number of treatments
+#' \item \code{t.stat}: a \eqn{1 \times |\mathcal A|} \code{vector} of \eqn{t}-statistics, where \eqn{|\mathcal A|} represents the number of treatments
+#' \item \code{p.value}: a \eqn{1 \times |\mathcal A|} \code{vector} of corresponding \eqn{p}-values, where \eqn{|\mathcal A|} represents the number of treatments
+#' \item \code{CI.left}: a \eqn{1 \times |\mathcal A|} \code{vector} of the left bounds of the 95\% as. confidence interval
+#' \item \code{CI.right}: a \eqn{1 \times |\mathcal A|} \code{vector} of the right bounds of the 95\% as. confidence interval
 #' \item \code{data}: an original data of the form \code{data.frame(Y, S, D, G.id, Ng, X)}
 #' \item \code{lin.adj}: a \code{data.frame} representing the covariates that were used in implementing linear adjustments
 #' }
@@ -153,18 +153,18 @@ sreg <- function(Y, S = NULL, D, G.id = NULL, Ng = NULL, X = NULL, HC1 = TRUE) {
 #' @param n a total number of observations in a sample
 #' @param Nmax a maximum size of generated clusters (maximum number of observations in a cluster)
 #' @param n.strata an integer specifying the number of strata
-#' @param tau.vec a numeric \eqn{1 \times |\mathcal A|} vector of treatment effects, where \eqn{|\mathcal A|} represents the number of treatments
-#' @param gamma.vec a numeric \eqn{1 \times 3} vector of parameters corresponding to covariates
+#' @param tau.vec a numeric \eqn{1 \times |\mathcal A|} \code{vector} of treatment effects, where \eqn{|\mathcal A|} represents the number of treatments
+#' @param gamma.vec a numeric \eqn{1 \times 3} \code{vector} of parameters corresponding to covariates
 #' @param cluster a \code{TRUE/FALSE} argument indicating whether the dgp should use a cluster-level treatment assignment or individual-level
 #' @param is.cov a \code{TRUE/FALSE} argument indicating whether the dgp should include covariates or not
 #'
 #' @return An object that is a `data.frame` with \eqn{n} observations containing the generated values of the following variables:
 #' \itemize{
-#' \item \code{Y}: a numeric \eqn{n \times 1} `vector` of observed outcomes
-#' \item \code{S}: a numeric \eqn{n \times 1} `vector` of strata indicators
-#' \item \code{D}: a numeric \eqn{n \times 1} `vector` of treatments indexed by \eqn{\{0, 1, 2, \ldots\}}, where \eqn{\code{D} = 0} denotes the control
-#' \item \code{G.id}: a numeric \eqn{n \times 1} `vector`of cluster indicators
-#' \item \code{X}: a `data.frame` with columns representing the covariate values for every observation
+#' \item \code{Y}: a numeric \eqn{n \times 1} \code{vector} of observed outcomes
+#' \item \code{S}: a numeric \eqn{n \times 1} \code{vector} of strata indicators
+#' \item \code{D}: a numeric \eqn{n \times 1} \code{vector} of treatments indexed by \eqn{\{0, 1, 2, \ldots\}}, where \eqn{\code{D} = 0} denotes the control
+#' \item \code{G.id}: a numeric \eqn{n \times 1} \code{vector} of cluster indicators
+#' \item \code{X}: a \code{data.frame} with columns representing the covariate values for every observation
 #' }
 #' @export
 #'
